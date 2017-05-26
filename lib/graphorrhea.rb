595ee05cdca2880f3b6d9099d98ae400f99941d1
Ray Walters <ray@icapitalnetwork.com>
2017-05-26 17:01:41 -0400

    Add words, sentence, and sentences
    - Return an array of words
    - Return a single sentence
    - Return an array of sentences
    - Add specs for all of the above
    
    Also refactor and use a stream of characters and a stream of words.

--- lib/graphorrhea.rb
+++ lib/graphorrhea.rb
@@ -1,26 +1,72 @@
 require 'securerandom'
 
 class Graphorrhea
-  DefaultWordLength = 5
+  DefaultWordLength     = 5
+  DefaultWordCount      = 5
+  DefaultSentenceCount  = 3
+
+  class << self
+    def word(num_letters = nil)
+      @@inst ||= new
+      @@inst.word(num_letters)
+    end
+
+    def words(num_words = nil, wlength = nil)
+      @@inst ||= new
+      @@inst.words(num_words, wlength)
+    end
+
+    def sentence(num_words = nil, wlength = nil)
+      words(num_words, wlength).join(' ') << '.'
+    end
+
+    def sentences(num_sentences = nil, slength = nil, wlength = nil)
+      @@inst ||= new
+      @@inst.sentences(num_sentences, slength, wlength)
+    end
+  end
 
   def initialize(seed = SecureRandom.random_number)
     @rng = Random.new(seed)
   end
 
-  def self.word(letters = nil)
-    @@inst ||= new
-    @@inst.word(letters)
+  def sentences(num_sentences = nil, slength = nil, wlength = nil)
+    num_sentences = DefaultSentenceCount if num_sentences.to_i <= 0
+    num_sentences.times.map{ self.class.sentence(sample(slength), wlength) }
   end
 
-  def word(letters = nil)
-    letters = DefaultWordLength  if letters.to_i <= 0
-    letters.times.map{ random_char }.join('')
+  def words(num_words = nil, wlength = nil)
+    num_words = DefaultWordCount if num_words.to_i <= 0
+    word_stream(wlength).take(num_words)
+  end
+
+  def word(num_letters = nil)
+    num_letters = DefaultWordLength if num_letters.to_i <= 0
+    char_stream.take(num_letters).join('')
   end
 
   private
 
   def random_char
-    CHARS.sample(random: @rng)
+    sample(CHARS)
+  end
+
+  def sample(from_array)
+    Array(from_array).sample(random: @rng)
+  end
+
+  def char_stream
+    Enumerator.new do |y|
+      loop { y.yield random_char }
+    end
+  end
+
+  def word_stream(wlength)
+    Enumerator.new do |y|
+      loop do
+        y.yield word(sample(wlength))
+      end
+    end
   end
 
   CHARS = ('a'..'z').to_a + ('A'..'Z').to_a + (0..9).to_a
--- spec/graphorrhea_spec.rb
+++ spec/graphorrhea_spec.rb
@@ -2,6 +2,255 @@ require 'rspec'
 require 'graphorrhea'
 
 describe Graphorrhea do
+  describe ".sentences" do
+    subject { described_class.sentences }
+
+    let(:default_count) { Graphorrhea::DefaultSentenceCount }
+
+    it "generates three sentences" do
+      expect(default_count).to eq 3
+    end
+
+    context "by default" do
+      it "generates the default numer of sentences" do
+        expect(subject.count).to  be default_count
+      end
+    end
+
+    context "with a sentence count specified" do
+      subject { described_class.sentences(wcount) }
+
+      context "with an explicit 'nil' count" do
+        let(:wcount) { nil }
+
+        it "returns default number of sentences" do
+          expect(subject.count).to eq default_count
+        end
+      end
+
+      context "with count of 0 specified" do
+        let(:wcount) { 0 }
+
+        it "returns default number of sentences" do
+          expect(subject.count).to eq default_count
+        end
+      end
+
+      context "with count of 10 specified" do
+        let(:wcount) { 10 }
+
+        it "returns ten sentences" do
+          expect(subject.count).to eq wcount
+        end
+      end
+    end
+
+    context "with sentence length specified" do
+      subject { described_class.sentences(5, words_per_sentence) }
+      let(:words_per_sentence)    { 5 }
+      let(:generated_words)       { subject.map{|i| i.sub(/\.$/, '').split(/ /)} }
+      let(:generated_word_counts) { generated_words.map(&:count) }
+
+      it "generates all sentences with that length" do
+        expect(generated_word_counts.uniq).to match_array([words_per_sentence])
+      end
+
+      context "specify a range for the sentence length" do
+        let(:words_per_sentence)  { wps_range }
+        let(:wps_range)           { (6...66) }
+
+        it "generates sentences of varying lengths" do
+          expect(generated_word_counts.min).not_to eq(generated_word_counts.max)
+        end
+
+        it "generates sentences within the specified range" do
+          # We have to 'splat' the array since 'cover' expects
+          # '.to cover(1,2,3)' and not '.to cover([1,2,3])'
+          expect(wps_range).to cover(*generated_word_counts)
+        end
+      end
+    end
+
+    context "with the word length specified" do
+      subject { described_class.sentences(5, 5, wlength) }
+      let(:wlength) { 9 }
+
+      let(:generated_word_lengths)  { generated_words.flat_map{|w_array| w_array.map(&:length).uniq } }
+      let(:generated_words)         { subject.map{|i| i.sub(/\.$/, '').split(/ /)} }
+
+      it "generates all words with that length" do
+        expect(generated_word_lengths.uniq).to match_array([wlength])
+      end
+
+      context "specify a range for the word length" do
+        let(:wlength)   { wl_range }
+        let(:wl_range)  { (6...66) }
+
+        it "generates words of varying lengths" do
+          expect(generated_word_lengths.min).not_to eq(generated_word_lengths.max)
+        end
+
+        it "generates words within the specified range" do
+          # We have to 'splat' the array since 'cover' expects
+          # '.to cover(1,2,3)' and not '.to cover([1,2,3])'
+          expect(wl_range).to cover(*generated_word_lengths)
+        end
+      end
+    end
+  end
+
+  describe ".sentence" do
+    subject { described_class.sentence }
+
+    let(:default_count) { Graphorrhea::DefaultWordCount }
+    let(:words_in_sentence) { subject.split(/ /) }
+
+    it "generates a five word sentence" do
+      expect(default_count).to eq 5
+    end
+
+    it "ends a sentence with a period" do
+      expect(subject).to match(/\.$/)
+    end
+
+    context "by default" do
+      it "generates the default numer of words" do
+        expect(words_in_sentence.count).to  be default_count
+      end
+    end
+
+    context "with a word count specified" do
+      subject { described_class.sentence(wcount) }
+
+      context "with an explicit 'nil' count" do
+        let(:wcount) { nil }
+
+        it "returns default number of words" do
+          expect(words_in_sentence.count).to eq default_count
+        end
+      end
+
+      context "with count of 0 specified" do
+        let(:wcount) { 0 }
+
+        it "returns default number of words" do
+          expect(words_in_sentence.count).to eq default_count
+        end
+      end
+
+      context "with count of 10 specified" do
+        let(:wcount) { 10 }
+
+        it "returns ten words" do
+          expect(words_in_sentence.count).to eq wcount
+        end
+      end
+    end
+
+    context "with word length specified" do
+      subject { described_class.sentence(5, word_length).tr('.', '') }
+      let(:word_length)       { 12 }
+      let(:generated_words)   { words_in_sentence.map(&:length) }
+
+      it "generates all words with that length" do
+        expect(generated_words.uniq).to match_array([word_length])
+      end
+
+      context "specify a range for the word length" do
+        let(:word_length)       { word_length_range }
+        let(:word_length_range) { (6...66) }
+
+        it "generates words of varying lengths" do
+          expect(generated_words.min).not_to eq(generated_words.max)
+        end
+
+        it "generates words within the specified range" do
+          # We have to 'splat' the array since 'cover' expects
+          # '.to cover(1,2,3)' and not '.to cover([1,2,3])'
+          expect(word_length_range).to cover(*generated_words)
+        end
+      end
+    end
+  end
+
+  describe ".words" do
+    subject { described_class.words }
+
+    let(:default_count) { Graphorrhea::DefaultWordCount }
+
+    it "generates five words by default" do
+      expect(default_count).to eq 5
+    end
+
+    context "by default" do
+      it "generates the default numer of words" do
+        expect(subject.count).to be default_count
+        expect(subject.size).to be default_count
+      end
+    end
+
+    context "with a word count specified" do
+      subject { described_class.words(wcount) }
+
+      context "with an explicit 'nil' count" do
+        let(:wcount) { nil }
+
+        it "returns default number of words" do
+          expect(subject.count).to eq default_count
+        end
+      end
+
+      context "with count of 0 specified" do
+        let(:wcount) { 0 }
+
+        it "returns default number of words" do
+          expect(subject.count).to eq default_count
+        end
+      end
+
+      context "with count of 10 specified" do
+        let(:wcount) { 10 }
+
+        it "returns ten words" do
+          expect(subject.count).to eq wcount
+        end
+      end
+
+      context "with count of 10000 specified" do
+        let(:wcount) { 10000 }
+
+        it "returns ten thousand words" do
+          expect(subject.count).to eq wcount
+        end
+      end
+    end
+
+    context "with word length specified" do
+      subject { described_class.words(5, word_length) }
+      let(:word_length)       { 12 }
+      let(:generated_words)   { subject.map(&:length) }
+
+      it "generates words of that length" do
+        expect(generated_words.uniq).to match_array([word_length])
+      end
+
+      context "specify a range for the word length" do
+        let(:word_length)       { word_length_range }
+        let(:word_length_range) { (6...66) }
+
+        it "should return words of varying lengths" do
+          expect(generated_words.min).not_to eq(generated_words.max)
+        end
+
+        it "should return words within the specified range" do
+          # We have to 'splat' the array since 'cover' expects
+          # '.to cover(1,2,3)' and not '.to cover([1,2,3])'
+          expect(word_length_range).to cover(*generated_words)
+        end
+      end
+    end
+  end
+
   describe ".word" do
     context "length of word returned" do
       subject { described_class.word }
@@ -20,9 +269,10 @@ describe Graphorrhea do
 
       context "with a length specified" do
         subject { described_class.word(wlength) }
-        let(:wlength) { nil }
 
         context "with an explicit 'nil' length" do
+          let(:wlength) { nil }
+
           it "returns a five character word" do
             expect(subject.length).to eq default_length
           end
@@ -32,7 +282,15 @@ describe Graphorrhea do
           let(:wlength) { 10 }
 
           it "returns a ten character word" do
-            expect(subject.length).to eq 10
+            expect(subject.length).to eq wlength
+          end
+        end
+
+        context "with length of 10000 specified" do
+          let(:wlength) { 10000 }
+
+          it "returns a 10000 character word" do
+            expect(subject.length).to eq wlength
           end
         end
 
@@ -49,21 +307,34 @@ describe Graphorrhea do
     context "content of word returned" do
       subject { described_class }
 
-      context "run twice in succession" do
-        it "returns different words each time" do
-          run1 = subject.word
-          run2 = subject.word
+      context "without specifying a seed" do
+        context "run twice in succession" do
+          it "returns different words each time" do
+            run1 = subject.word
+            run2 = subject.word
 
-          expect(run1).not_to eq run2
+            expect(run1).not_to eq run2
+          end
         end
       end
 
-      context "initialized with the same seed" do
-        it "returns identical words each time" do
-          run1 = subject.new(1001).word
-          run2 = subject.new(1001).word
+      context "specifying a seed" do
+        context "initialized with the same seed" do
+          it "returns identical words each time" do
+            run1 = subject.new(1001).word
+            run2 = subject.new(1001).word
 
-          expect(run1).to eq run2
+            expect(run1).to eq run2
+          end
+        end
+
+        context "initialized with different seeds" do
+          it "returns different words each time" do
+            run1 = subject.new(42).word
+            run2 = subject.new(1138).word
+
+            expect(run1).not_to eq run2
+          end
         end
       end
     end
